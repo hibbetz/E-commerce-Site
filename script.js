ae55928a0178b4f2756673d05c95dfab877011ea
hibbetz <42220074+hibbetz@users.noreply.github.com>
2021-01-06 15:34:30 +0000

    Adding shopping cart main page and functionality inc. tweaks to existing pages

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log("loaded")
-  const furniturename = document.getElementById("furniturename");
-  const furnituredescription = document.getElementById("description");
-  const furnitureprice = document.getElementById("price");
-
-  var item = document.getElementByClass("list-group shadow").lastChild;
-  var clone = item.cloneNode();
+  var item = document.getElementsByClassName("list-group-item")[0];
+  var clone = item.cloneNode(true);
   /*Id of the element below which you want to clone*/
-  document.getElementByClass("list-group-item").appendChild(clone);
+  document.getElementsByClassName("list-group shadow")[0].appendChild(clone);
 
   let furnitureRequest = new XMLHttpRequest();
   furnitureRequest.open("GET","http://localhost:3000/api/furniture");
   furnitureRequest.onload = function() {
       var furnitureData = JSON.parse(furnitureRequest.response);
-         furniturename.textContent= furnitureData[1].name
-         furnituredescription.textContent= furnitureData[1].description
-         furnitureprice.textContent= furnitureData[1].price
+      createList(furnitureData);
     };
   furnitureRequest.send();
 }, false);
 //clone and append through JS//
+
+const createList = (furnitureData) => {
+    for (index in furnitureData) {
+      const item = furnitureData[index]
+      var itemDiv = document.getElementsByClassName("list-group-item")[0];
+      var clone = itemDiv.cloneNode(true);
+      const furniturename = clone.querySelector(".furniture-name");
+      const furnituredescription = clone.querySelector(".description");
+      const furnitureprice = clone.querySelector(".price");
+      furniturename.innerText= item.name
+      furnituredescription.innerText= item.description
+      furnitureprice.innerText= item.price
+      document.getElementsByClassName("list-group shadow")[0].appendChild(clone);
+    }
+}

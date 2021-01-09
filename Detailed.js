4addc6c60c4723019bd06d40607300d21f720822
hibbetz <42220074+hibbetz@users.noreply.github.com>
2021-01-09 11:09:39 +0000

    Changes to Java for product pages

--- Detailed.js
+++ Detailed.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
   var clone = item.cloneNode(true);
   /*Id of the element below which you want to clone*/
   document.getElementsByClassName("productscontainer")[0].appendChild(clone);
-
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get('id');
   let furnitureRequest = new XMLHttpRequest();
-  furnitureRequest.open("GET","http://localhost:3000/api/furniture");
+  furnitureRequest.open("GET",`http://localhost:3000/api/furniture/${myParam}`);
   furnitureRequest.onload = function() {
       var furnitureData = JSON.parse(furnitureRequest.response);
+      console.log("myParam", furnitureData)
       createList(furnitureData);
     };
   furnitureRequest.send();
@@ -15,20 +17,17 @@ document.addEventListener('DOMContentLoaded', function() {
 //clone and append through JS//
 
 const createList = (furnitureData) => {
-    for (index in furnitureData) {
       const item = furnitureData[index]
       var itemDiv = document.getElementsByClassName("productslisting")[0];
       var clone = itemDiv.cloneNode(true);
       const furniturename = clone.querySelector(".furniture-name");
+      console.log("furniturename", furniturename)
       furniturename.setAttribute('href',`Detailed.html?id=${item._id}`);
       const furnituredescription = clone.querySelector(".description");
       const furnitureprice = clone.querySelector(".price");
-      const furnitureimage = clone.querySelector(".furnitureImage");
       furniturename.innerText= item.name
       furnituredescription.innerText= item.description
       furnitureprice.innerText= item.price
-      furnitureimage.src= item.imageUrl
-      console.log("furniturename", furnitureimage)
       document.getElementsByClassName("productscontainer")[0].appendChild(clone);
     }
 }

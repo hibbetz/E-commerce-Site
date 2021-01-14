5a03608f0a18e2c114930cc3df12611c341a36a1
hibbetz <42220074+hibbetz@users.noreply.github.com>
2021-01-14 07:32:32 +0000

    Detailed Page working as intended
    hell yeah

--- Detailed.js
+++ Detailed.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var item = document.getElementsByClassName("productslisting")[0];
-  var clone = item.cloneNode(true);
-  /*Id of the element below which you want to clone*/
-  document.getElementsByClassName("productscontainer")[0].appendChild(clone);
+  var item = document.getElementsByClassName("productslisting");
+  document.getElementsByClassName("productscontainer")
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get('id');
   let furnitureRequest = new XMLHttpRequest();
@@ -17,17 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
 //clone and append through JS//
 
 const createList = (furnitureData) => {
-      const item = furnitureData[index]
-      var itemDiv = document.getElementsByClassName("productslisting")[0];
-      var clone = itemDiv.cloneNode(true);
-      const furniturename = clone.querySelector(".furniture-name");
+      const item = furnitureData
+      var itemDiv = document.getElementsByClassName("productslisting");
+      const furniturename = document.querySelector(".furniture-name");
       console.log("furniturename", furniturename)
       furniturename.setAttribute('href',`Detailed.html?id=${item._id}`);
-      const furnituredescription = clone.querySelector(".description");
-      const furnitureprice = clone.querySelector(".price");
+      const furnituredescription = document.querySelector(".description");
+      const furnitureprice = document.querySelector(".price");
+      const furnitureimage = document.querySelector(".furnitureImage");
       furniturename.innerText= item.name
       furnituredescription.innerText= item.description
       furnitureprice.innerText= item.price
-      document.getElementsByClassName("productscontainer")[0].appendChild(clone);
-    }
+      furnitureimage.src= item.imageUrl
+      document.getElementsByClassName("productscontainer");
 }
